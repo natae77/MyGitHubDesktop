cc03b5989ec2b3e048bbffbe71ac0aecf923c0cb
natae77 <natae77@gmail.com>
2026-03-25 09:33:07 +0900

    GithubDesktopN으로 파일명, 설치명 정리

--- script/dist-info.ts
+++ script/dist-info.ts
@@ -95,7 +95,7 @@ export function getWindowsDeltaNugetPackagePath() {
 }
 
 export function getWindowsIdentifierName() {
-  return 'GitHubDesktop'
+  return 'GitHubDesktopN'
 }
 
 export function getBundleSizes() {
